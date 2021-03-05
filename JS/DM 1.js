0342f02dc0845bc486f2506ddda7f62dc00ab33c
alexey-fadeev <78476821+alexey-fadeev@users.noreply.github.com>
2021-03-06 01:30:29 +0400

    добавлена функция пересечения

--- JS/DM 1.js
+++ JS/DM 1.js
@@ -1,4 +1,4 @@
-var firstMass = [], secondMass = [], unMass = [];
+var firstMass = [], secondMass = [], unMass = [],intMass = [];
 
 function print(element) {
 	var div = document.createElement('div');
@@ -22,16 +22,31 @@ function myUnion() {
 	Repeat(unMass, secondMass);
 	print(`<br/>Результат объединения множеств: ${unMass}`);
 }
+function myIntersection() {
+	for(var i = 0; i < firstMass.length; i++){
+		var cnt = 0;
+		for(var j = 0; j < secondMass.length; j++){
+			if(firstMass[i] == secondMass[j]){
+				cnt++;
+				break;
+			}
+		}
+		if(cnt > 0){
+			intMass.push(firstMass[i]);
+		}
+	}
+	print(`<br/>Результат пересечения множеств: ${intMass}`)
+}
 function functions() {
 	FirstArray = document.getElementById('FirstArray').value;
 	SecondArray = document.getElementById('SecondArray').value;
 	FirstArray = FirstArray.split(' ');
 	SecondArray = SecondArray.split(' ');
-
 	Repeat(firstMass, FirstArray);
 	Repeat(secondMass, SecondArray);
 	print(`Первое множество: ${firstMass} <br/><br/>Второе множество: ${secondMass}`)
 	myUnion();
+	myIntersection();
 	firstMass =[];
 	secondMass =[];
 }
